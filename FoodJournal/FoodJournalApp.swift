bc4ee67fba7134d59f8d5f25399410ac0163829d
Grace Zhu <gracezhu059@gmail.com>
2023-01-20 10:24:22 -0600

    Initial Commit

---
+++ FoodJournal/FoodJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodJournalApp.swift
+//  FoodJournal
+//
+//  Created by Grace Zhu on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
